7d8ec58920e8993aef0919b25b2dd0b194e739c5
m <m@bitsnbites.eu>
2019-09-23 08:56:02 +0200

    us3get: Convert the code to C90

--- tools/us3get.c
+++ tools/us3get.c
@@ -1,26 +1,29 @@
-//--------------------------------------------------------------------------------------------------
-// Copyright (c) 2019 Marcus Geelnard
-//
-// This software is provided 'as-is', without any express or implied warranty. In no event will the
-// authors be held liable for any damages arising from the use of this software.
-//
-// Permission is granted to anyone to use this software for any purpose, including commercial
-// applications, and to alter it and redistribute it freely, subject to the following restrictions:
-//
-//  1. The origin of this software must not be misrepresented; you must not claim that you wrote
-//     the original software. If you use this software in a product, an acknowledgment in the
-//     product documentation would be appreciated but is not required.
-//
-//  2. Altered source versions must be plainly marked as such, and must not be misrepresented as
-//     being the original software.
-//
-//  3. This notice may not be removed or altered from any source distribution.
-//--------------------------------------------------------------------------------------------------
+/*--------------------------------------------------------------------------------------------------
+ * Copyright (c) 2019 Marcus Geelnard
+ *
+ * This software is provided 'as-is', without any express or implied warranty. In no event will the
+ * authors be held liable for any damages arising from the use of this software.
+ *
+ * Permission is granted to anyone to use this software for any purpose, including commercial
+ * applications, and to alter it and redistribute it freely, subject to the following restrictions:
+ *
+ *  1. The origin of this software must not be misrepresented; you must not claim that you wrote
+ *     the original software. If you use this software in a product, an acknowledgment in the
+ *     product documentation would be appreciated but is not required.
+ *
+ *  2. Altered source versions must be plainly marked as such, and must not be misrepresented as
+ *     being the original software.
+ *
+ *  3. This notice may not be removed or altered from any source distribution.
+ *------------------------------------------------------------------------------------------------*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <us3/us3.h>
+
+#define BUFFER_SIZE 32768
+static char s_buffer[BUFFER_SIZE];
 
 static void show_usage(const char* program) {
   fprintf(stderr, "Usage: %s [options] URL [FILE]\n\n", program);
@@ -37,14 +40,21 @@ static void show_usage(const char* program) {
 }
 
 int main(const int argc, const char** argv) {
-  // Parse arguments.
   const char* program = argv[0];
   const char* url = NULL;
   const char* file_name = NULL;
   const char* access_key = NULL;
   const char* secret_key = NULL;
   int bad_args = 0;
-  for (int i = 1; i < argc; ++i) {
+  int i;
+  int output_to_file;
+  FILE* file;
+  int exit_status = EXIT_FAILURE;
+
+  /* Parse arguments. */
+  us3_handle_t s3_handle;
+
+  for (i = 1; i < argc; ++i) {
     if ((strcmp(argv[i], "--help") == 0) || (strcmp(argv[i], "-h") == 0)) {
       show_usage(program);
       exit(EXIT_SUCCESS);
@@ -98,8 +108,7 @@ int main(const int argc, const char** argv) {
     exit(EXIT_FAILURE);
   }
 
-  // Open the S3 stream.
-  us3_handle_t s3_handle;
+  /* Open the S3 stream. */
   {
     us3_status_t open_status = us3_open_url(
         url, access_key, secret_key, US3_READ, US3_NO_TIMEOUT, US3_NO_TIMEOUT, &s3_handle);
@@ -109,9 +118,9 @@ int main(const int argc, const char** argv) {
     }
   }
 
-  // Open the output file.
-  int output_to_file = 0;
-  FILE* file = stdout;
+  /* Open the output file. */
+  output_to_file = 0;
+  file = stdout;
   if (file_name != NULL) {
     file = fopen(file_name, "wb");
     if (file == NULL) {
@@ -122,30 +131,28 @@ int main(const int argc, const char** argv) {
     output_to_file = 1;
   }
 
-  // Read & write...
-  int exit_status = EXIT_FAILURE;
-  #define BUF_SIZE 32768
-  static char buf[BUF_SIZE];
+  /* Read & write... */
   while (1) {
     size_t bytes_in_buf;
-    us3_status_t read_status = us3_read(s3_handle, &buf[0], BUF_SIZE, &bytes_in_buf);
+    size_t bytes_written;
+    us3_status_t read_status = us3_read(s3_handle, &s_buffer[0], BUFFER_SIZE, &bytes_in_buf);
     if (read_status != US3_SUCCESS) {
       fprintf(stderr, "*** Read error: %s\n", us3_status_str(read_status));
       break;
     }
     if (bytes_in_buf == 0) {
-      // Done!
+      /* Done! */
       exit_status = EXIT_SUCCESS;
       break;
     }
-    size_t bytes_written = fwrite(&buf[0], 1, bytes_in_buf, file);
+    bytes_written = fwrite(&s_buffer[0], 1, bytes_in_buf, file);
     if (bytes_written != bytes_in_buf) {
       fprintf(stderr, "*** Write error\n");
       break;
     }
   }
 
-  // Close the handles.
+  /* Close the handles. */
   if (output_to_file) {
     fclose(file);
   }
